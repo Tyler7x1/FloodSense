1441d6511e26d97833757b66a61375f2361c896e
Tyler <jpm022504@gmail.com>
2024-12-12 19:29:00 +0530

    Fixed

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
 const { connectToMongo } = require('./connect');
-const { fetchData, pushData, latestData, serverListen } = require('./controllers/dataController');
-
+const { fetchData, pushData, getAllData } = require('./controllers/dataController');
+const Data = require('./models/dataModel');
+const express = require('express');
 const app = express();
+const http = require('http');
+const cors = require('cors');
+const server = http.createServer(app);
+const socketIo = require('socket.io');
+const io = socketIo(server);
 
 const uri = process.env.MONGO_URI
 const port = process.env.PORT;
@@ -18,8 +22,26 @@ app.set('views', './views');   // Define the views folder
 connectToMongo(uri);
 
 app.get('/', fetchData);
-latestData;
+app.get('/data', getAllData);
+io.on('connection', async (socket) => {
+    console.log('A user connected');
+    try {
+      // Fetch the most recent data from the database and send it to the connected client
+      const latestData = await Data.findOne().sort({ timestamp: -1 });
+      if (latestData) {
+        socket.emit('newData', latestData); // Emit data to client
+      }
+    } catch (err) {
+      console.error("Error fetching latest data:", err);
+    }
+    // Listen for disconnection events
+    socket.on('disconnect', () => {
+      console.log('User disconnected');
+    });
+  });
 app.post('/data', express.json(), pushData);
 
 // Start the server
-serverListen(port);
+server.listen(port, ()=> {
+    console.log(`Server listening on http://localhost:${port}/`)
+});
